33c51347995a61cbe40ba533de2e72aa5f2e48ba
Silvia Bolaña <168044833+silviabolana2006@users.noreply.github.com>
2025-06-29 17:02:39 -0300

    Actualiza documentos

---
+++ Autoevaluacion_de_desempenio_laboral/experto_general/base.py
@@ -0,0 +1,31 @@
+base_conocimiento = {
+    "asistencia": {
+        "pregunta": "¿Tuvo asistencia completa?",
+        "respuestas": {
+            "sí": 100,
+            "falta_justificada": 100,
+            "falta_injustificada": 0,
+            "tardanza": 0
+        }
+    },
+    "objetivos": {
+        "pregunta": "¿Cumplió los objetivos?",
+        "respuestas": {
+            "sí": 100,
+            "no_grupal": 50,
+            "no_personal": 0
+        }
+    },
+    "desempeño": {
+        "pregunta": "¿Qué porcentaje cumple con su puesto?",
+        "tipo": "porcentaje"
+    },
+    "tiempo": {
+        "pregunta": "¿Qué porcentaje representa su gestión del tiempo?",
+        "tipo": "rango"
+    },
+    "equipo": {
+        "pregunta": "¿Qué porcentaje representa su colaboración en equipo?",
+        "tipo": "rango"
+    }
+}
