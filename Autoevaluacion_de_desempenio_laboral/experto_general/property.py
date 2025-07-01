078d57ccca0cfc92b430a7a8de9205970cde5180
Silvia Bolaña <168044833+silviabolana2006@users.noreply.github.com>
2025-07-01 01:25:37 -0300

    Actualiza documentos

--- Autoevaluacion_de_desempenio_laboral/experto_general/property.py
+++ Autoevaluacion_de_desempenio_laboral/experto_general/property.py
@@ -1,4 +1,5 @@
 
+
 class EvaluacionEmpleado:
     """
     Clase para encapsular los datos de una evaluación de empleado.
@@ -8,7 +9,7 @@ class EvaluacionEmpleado:
         self.id_empleado = id_empleado
         self.nombre_empleado = nombre_empleado
         self.respuestas = {}  # Diccionario para almacenar las respuestas del usuario
-        self.puntajes_individuales = [] # Lista de puntajes para cada criterio
+        self.puntajes_individuales = [] 
         self.puntaje_final = 0       # Puntaje promedio final
         self.interpretacion = ""    # Interpretación textual del puntaje final
         self.explicacion_proceso = [] # Lista de cadenas explicando la inferencia
@@ -19,12 +20,13 @@ class EvaluacionEmpleado:
         """
         self.respuestas[clave_pregunta] = valor_respuesta
 
-    def set_resultados_inferencia(self, puntaje_final, puntajes_individuales, explicacion):
+    def set_resultados_inferencia(self, puntaje_final, puntajes_detallados, explicacion):
         """
         Establece los resultados obtenidos del motor de inferencia.
+        puntajes_detallados ahora es una lista de diccionarios con {'clave', 'pregunta', 'puntaje'}
         """
         self.puntaje_final = puntaje_final
-        self.puntajes_individuales = puntajes_individuales
+        self.puntajes_individuales = puntajes_detallados 
         self.explicacion_proceso = explicacion
 
     def set_interpretacion(self, interpretacion):
@@ -42,7 +44,7 @@ class EvaluacionEmpleado:
             "id_empleado": self.id_empleado,
             "nombre_empleado": self.nombre_empleado,
             "respuestas": self.respuestas,
-            "puntajes_individuales": self.puntajes_individuales,
+            "puntajes_individuales": self.puntajes_individuales, # Se usa el nuevo formato
             "puntaje_final": self.puntaje_final,
             "interpretacion": self.interpretacion,
             "explicacion_proceso": self.explicacion_proceso
